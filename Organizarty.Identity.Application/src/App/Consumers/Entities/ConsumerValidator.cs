26307ced5472fdc664f340a6c642d459237844d9
lu-css <dossantossilvalucas963@gmail.com>
2024-01-01 17:04:03 -0300

    [feat] Password check

--- Organizarty.Identity.Application/src/App/Consumers/Entities/ConsumerValidator.cs
+++ Organizarty.Identity.Application/src/App/Consumers/Entities/ConsumerValidator.cs
@@ -1,5 +1,7 @@
 using FluentValidation;
+
 using Organizarty.Identity.Application.App.Consumers.Entities;
+using Organizarty.Utils.Validations.CustomValidations;
 
 namespace Organizarty.Application.App.Users.Entities;
 
@@ -10,7 +12,7 @@ public class ConsumerValidator : AbstractValidator<Consumer>
         RuleFor(x => x.Email).EmailAddress().NotNull();
         RuleFor(x => x.UserName).Length(5, 50).NotNull();
         RuleFor(x => x.Fullname).Length(5, 80).NotNull();
-        RuleFor(x => x.HashedPassword).MinimumLength(8).NotNull();
+        RuleFor(x => x.HashedPassword).Password();
 
         RuleFor(x => x.CPF).Length(11).When(x => !string.IsNullOrWhiteSpace(x.CPF));
     }
--- Organizarty.Identity.Application/src/App/Consumers/UseCases/RegisterConsumerUseCase.cs
+++ Organizarty.Identity.Application/src/App/Consumers/UseCases/RegisterConsumerUseCase.cs
@@ -2,7 +2,7 @@ using FluentValidation;
 using Organizarty.Identity.Adapters.Cryptographies;
 using Organizarty.Identity.Application.App.Consumers.Data;
 using Organizarty.Identity.Application.App.Consumers.Entities;
-using Organizarty.Utils.Extensions;
+using Organizarty.Utils.Validations.Extensions;
 
 namespace Organizarty.Identity.Application.App.Consumers.UseCases;
 
@@ -13,14 +13,12 @@ public class RegisterConsumerUseCase
     private readonly ICryptographys _cryptograph;
     private readonly IConsumerRepository _consumerRepository;
     private readonly IValidator<Consumer> _userValidator;
-    private readonly SendEmailConfirmationUseCase _sendEmailConfirmation;
 
-    public RegisterConsumerUseCase(ICryptographys cryptograph, IConsumerRepository consumerRepository, IValidator<Consumer> userValidator, SendEmailConfirmationUseCase sendEmailConfirmation)
+    public RegisterConsumerUseCase(ICryptographys cryptograph, IConsumerRepository consumerRepository, IValidator<Consumer> userValidator)
     {
         _cryptograph = cryptograph;
         _consumerRepository = consumerRepository;
         _userValidator = userValidator;
-        _sendEmailConfirmation = sendEmailConfirmation;
     }
 
     public async Task<Consumer> Execute(SimpleRegister userDto)
@@ -35,8 +33,6 @@ public class RegisterConsumerUseCase
         consumer.Salt = salt;
 
         var u = await _consumerRepository.Save(consumer);
-
-        await _sendEmailConfirmation.Execute(userDto.Email);
 
         return u;
     }
