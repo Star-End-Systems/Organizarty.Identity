eb48d0305b376d528b201b7b9f4e9dae1a9ea484
lu-css <dossantossilvalucas963@gmail.com>
2024-01-06 12:13:38 -0300

    [feat] show email confirmation status in user repsonse

--- Organizarty.Identity.UI/Controllers/Consumers/ConsumerControllerDTO.cs
+++ Organizarty.Identity.UI/Controllers/Consumers/ConsumerControllerDTO.cs
@@ -2,10 +2,10 @@ using Organizarty.Domain.Entities.Consumers;
 
 namespace Organizarty.Identity.UI.Controllers;
 
-public record SecureConsumerResponse(string Id, string Fullname, DateTime BirthDate, string Email, DateTime CreatedAt)
+public record SecureConsumerResponse(string Id, string Fullname, DateTime BirthDate, string Email, bool EmailConfirmed, DateTime CreatedAt)
 {
     public static SecureConsumerResponse FromEntity(Consumer x)
-      => new(x.Id, x.Fullname, x.BirthDate, x.Email, x.CreatedAt);
+      => new(x.Id, x.Fullname, x.BirthDate, x.Email, x.EmailConfirmed, x.CreatedAt);
 }
 
 public record LoginUserResponse(SecureConsumerResponse consumer, string token);
